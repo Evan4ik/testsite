b50178796bf59b5157992babf3307df446b2c4ef
Evan4ik <64590088+Evan4ik@users.noreply.github.com>
2021-05-06 09:44:18 +0300

    Update python.py

--- python.py
+++ python.py
@@ -2,3 +2,8 @@ casino = "in my hair"
 print("pyth on my ass")
 casino = input("enter whats where")
 print("pyth" + casino)
+number1 = input("we are")
+number2 = input("number 1")
+
+result = float(number1) * float(number2)
+print("result)"
