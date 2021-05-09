2cb4fd6b3d1b22c2a66dff20a162be64eec264bc
Evan4ik <64590088+Evan4ik@users.noreply.github.com>
2021-05-09 23:42:10 +0300

    Create csharp.cs

---
+++ csharp.cs
@@ -0,0 +1,21 @@
+namespace Pee
+{
+class PooPoo 
+{
+ public string funny;
+ public int amountSaid;
+ public dump(string comedy, int dumplings)
+ {
+  funny = comedy;
+  amountSaid = dumplings;
+}
+}
+ class Program
+ }
+ static void Main(string[] args)
+ {
+  PooPoo joke = new dump(haha, 1)
+}
+{
+
+}
