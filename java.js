d60384b0740204df9e7aaebbbc31686061f43dd8
Evan4ik <64590088+Evan4ik@users.noreply.github.com>
2021-05-10 10:06:46 +0300

    Update java.js

--- java.js
+++ java.js
@@ -1,10 +1,19 @@
 
 var number = 5; // in-line comment
+var technologic = ["Buy it," "use it", "break it", "fix it", "trash it", "change it", "mail", "upgrade it"]
 function Lol() {
 number += 5;
+if (number >= 15) {
+  console.log("PYTHON SUCKS, CASINO IN MY CHAIR!!!")
 }
+let i;
 console.log(number)
 Lol()
 Lol()
+Lol()
+for (i = 0, i <= 9, i++) {
+ console.log(technologic[i]) 
+}
+
 console.log(number)
 
