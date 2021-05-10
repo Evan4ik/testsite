03babb92ec88d6b7237e63d2543596322c257e4d
Evan4ik <64590088+Evan4ik@users.noreply.github.com>
2021-05-10 10:03:20 +0300

    Update csharp.cs

--- csharp.cs
+++ csharp.cs
@@ -10,6 +10,16 @@ class PooPoo
   amountSaid = dumplings;
 }
 }
+class Incredible
+{
+public string thing;
+public int goodAmount;
+public amaze(string cool, int coolness)
+{
+ thing = cool
+  goodAmount = coolness
+}
+}
  class Program
  }
  static void Main(string[] args)
@@ -18,6 +28,8 @@ class PooPoo
   string[] humanAfterAll = {"Human After All", "Prime Time Of Your Life", "Robot Rock", "Steam Machine", "Make Love", "The Brainwasher", "On/Off", "Television Rules the Nation", "Technologic", "Emotion"}
   PooPoo joke = new dump(haha, 1)
   Console.WriteLine(joke)
+  PooPoo gameEngine = new dump(unity, 0) //joke unity is good plz dont murder me
+  Incredible gameEngine2 = new amaze(godot, 100)
   Console.WriteLine("best = " + humanAfterAll[6])
   Console.ReadLine();
 }
